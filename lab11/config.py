f815942d3681371b287c355c94e15c532cf27834
kamilleeka <123452245+kamilleeka@users.noreply.github.com>
2023-05-02 16:42:30 +0600

    Add files via upload

---
+++ lab11/config.py
@@ -0,0 +1,5 @@
+database='phoneb'
+user = 'postgres'
+host = 'localhost'
+port = 5432,
+password = '1234567'
---
+++ lab11/create_table.py
@@ -0,0 +1,19 @@
+import psycopg2
+from config import *
+conn = psycopg2.connect(
+    host=host,
+    user=user,
+    password=password,
+    database=database
+)
+conn.autocommit = True
+cursor = conn.cursor()
+with conn.cursor() as cursor:
+    cursor.execute(
+        """CREATE TABLE IF NOT EXISTS phonebook(
+            Name text PRIMARY KEY,
+            number text NOT NULL);"""
+    )
+
+cursor.close()
+conn.close()
